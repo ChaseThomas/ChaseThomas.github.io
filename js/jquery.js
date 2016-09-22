45602c039b71657843ee7b01301bf59c0462ffbe
Chase Thomas <chase@chasethomas.com>
2016-09-22 16:36:52 -0400

    Cascading animations

---
+++ js/jquery.js
@@ -0,0 +1,7 @@
+$(document).ready(function(){
+    $('#page_effect').fadeIn(1500, function(){
+        $("#github").fadeIn(1000);
+        $("#linkedin").delay(500).fadeIn(1000);
+        $("#email").delay(1000).fadeIn(1000);
+    });
+});
