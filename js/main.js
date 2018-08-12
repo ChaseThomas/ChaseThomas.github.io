273c2eb41be89052efe55dd052a7cd6d537e92f0
Chase Thomas <chase@chasethomas.com>
2018-08-12 15:55:05 -0700

    changed pic, description, added insta

--- js/main.js
+++ js/main.js
@@ -2,6 +2,7 @@ $(document).ready(function(){
     $("#page_effect").fadeIn(1500, function(){
         $("#github").animate({opacity:1}, 1000);
         $("#linkedin").delay(500).animate({opacity:1}, 1000);
-        $("#email").delay(1000).animate({opacity:1}, 1000);
+        $("#instagram").delay(1000).animate({opacity:1}, 1000);
+        $("#email").delay(1500).animate({opacity:1}, 1000);
     });
 });
