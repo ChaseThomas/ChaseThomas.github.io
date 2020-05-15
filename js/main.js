13637fc772a4219cf08e46276e77fac15811f0f4
Chase Thomas <chase@chasethomas.com>
2020-05-14 22:01:48 -0400

    Added portfolio

--- js/main.js
+++ js/main.js
@@ -1,8 +1,11 @@
 $(document).ready(function(){
-    $("#page_effect").fadeIn(1500, function(){
+    
+    $("#page_effect").delay(500).fadeIn(1500, function(){
         $("#github").animate({opacity:1}, 1000);
         $("#linkedin").delay(500).animate({opacity:1}, 1000);
         $("#instagram").delay(1000).animate({opacity:1}, 1000);
         $("#email").delay(1500).animate({opacity:1}, 1000);
+        $(".myButton").delay(2000).animate({opacity:1}, 1000);
     });
+    $("#portfolio").fadeIn(1500);
 });
