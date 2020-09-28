d8f5886b098861db340cd4ad74970f4bb403e323
Chase Thomas <chase@chasethomas.com>
2020-09-28 12:15:10 -0400

    added twitter

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 $(document).ready(function(){
     
     $("#page_effect").delay(500).fadeIn(1500, function(){
-        $("#github").animate({opacity:1}, 1000);
+        $("#twitter").animate({opacity:1}, 1000);
         $("#linkedin").delay(500).animate({opacity:1}, 1000);
         $("#instagram").delay(1000).animate({opacity:1}, 1000);
         $("#email").delay(1500).animate({opacity:1}, 1000);
