3891933210aaa22611a6be106a701093ddd3a5c0
Chase Thomas <chase@chasethomas.com>
2021-10-26 13:03:49 -0400

    Update main.js

--- js/main.js
+++ js/main.js
@@ -5,7 +5,7 @@ $(document).ready(function(){
         $("#linkedin").delay(500).animate({opacity:1}, 1000);
         $("#instagram").delay(1000).animate({opacity:1}, 1000);
         $("#email").delay(1500).animate({opacity:1}, 1000);
-        $(".myButton").delay(2000).animate({opacity:1}, 1000);
+        //$(".myButton").delay(2000).animate({opacity:1}, 1000);
     });
     $("#portfolio").fadeIn(1500);
 });
